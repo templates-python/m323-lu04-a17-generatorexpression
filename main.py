9fce6f494e69ca27f76fdee5c848f485342ddc85
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-20 10:48:17 +0200

    done

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def get_user_by_id(user_list, user_id):
     Returns:
     - dict: Das Dictionary des Benutzers mit der angegebenen ID. Wenn kein Benutzer gefunden wird, gibt die Funktion None zurück.
     """
-    pass
+    return next((user for user in user_list if user['id'] == user_id), None)
 
 
 if __name__ == '__main__':
